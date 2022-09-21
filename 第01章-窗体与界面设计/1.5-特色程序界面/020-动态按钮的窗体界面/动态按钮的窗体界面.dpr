97a01f4ec464dd1a56e04e39261e6cff4dbcdbd5
Leon <Liangz.org@gmail.com>
2022-09-21 22:58:22 +0800

    feat: add 020-动态按钮的窗体界面

---
+++ 第01章-窗体与界面设计/1.5-特色程序界面/020-动态按钮的窗体界面/动态按钮的窗体界面.dpr
@@ -0,0 +1,14 @@
+﻿program 动态按钮的窗体界面;
+
+uses
+  Vcl.Forms,
+  RSGLXT in 'RSGLXT.PAS' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm74, Form74);
+  Application.Run;
+end.
